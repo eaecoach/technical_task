0333f43205932a1deef39e235ce56574faa60452
eaecoach <eae@coachsolutions.com>
2024-11-13 10:54:24 +0100

    Update add_device_steps.py

--- features/steps/add_device_steps.py
+++ features/steps/add_device_steps.py
@@ -7,7 +7,7 @@ import json
 @given("I have added a device")
 def test_add_device_step(context):
     header = {'Content-Type': 'application/json'}
-    body = {"DeviceName": "device_3"}
+    body = {"DeviceName": "device_3_for_testing_behave"}
     context.response = requests.post('http://localhost:8080/device/add', data=json.dumps(body), headers=header)
     assert context.response.json() == 'Successfully added device - device_3!', 'Add device response incorrect! ' + str(context.response.json())
     assert context.response.status_code == 200, 'Add device status code incorrect!'
@@ -23,7 +23,7 @@ def check_add_device_response(context):
     for i in range(len(context.reservation_response)):
         if 'device_3' == context.reservation_response[i][0]:
             assert context.reservation_response[i] == {
-        "DeviceName": 'device_3',
+        "DeviceName": 'device_3_for_testing_behave',
         "ReservationStatus": "free",
         "ReservationPeriod": {
             "From": "none",
